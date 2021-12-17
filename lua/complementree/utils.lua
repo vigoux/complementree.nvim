bef8d584b9200c93a8448923ef02b063e1cb5975
Senghan Bright <senghan.bright@deltaprojects.com>
2021-12-17 09:16:20 +0100

    refactor: remove remains of original scan_dir func

--- lua/complementree/utils.lua
+++ lua/complementree/utils.lua
@@ -1,9 +1,5 @@
 local M = {}
 local api = vim.api
-local uv = vim.loop
-
-local os = string.lower(jit.os)
-local os_sep = (os == 'linux' or os == 'osx' or os == 'bsd') and '/' or '\\'
 
 function M.feed(codes)
   api.nvim_feedkeys(api.nvim_replace_termcodes(codes, true, true, true), 'm', true)
@@ -13,7 +9,7 @@ function M.cword(complete_item)
   return (complete_item.word or complete_item.abbr)
 end
 
-M.make_relative_path = function(path, root)
+function M.make_relative_path(path, root)
   local baselen = #root
   if path:sub(0, baselen) == root then
     path = path:sub(baselen + 2)
