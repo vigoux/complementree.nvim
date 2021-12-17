9e3a94746390eb1e3ed2c8ff0335f18378efae79
Thomas Vigouroux <thomas.vigouroux@protonmail.com>
2021-12-17 09:31:55 +0100

    feat(sources): use options for every source

--- lua/complementree/sources.lua
+++ lua/complementree/sources.lua
@@ -35,124 +35,135 @@ local function cached(kind, func)
   end
 end
 
-M.luasnip_matches = cached('luasnip', function(line_to_cursor, _)
-  local snippets = require('luasnip').available()
+-- Options:
+--
+-- filetype: forces the filetype as a source
+-- exclude_default: don't include the default snippets
+function M.luasnip_matches(opts)
+  return cached('luasnip', function(line_to_cursor, _)
+    local snippets = require('luasnip').available()
 
-  local pref_start = line_to_cursor:find '%w*$'
-  local prefix = line_to_cursor:sub(pref_start)
+    local pref_start = line_to_cursor:find '%w*$'
+    local prefix = line_to_cursor:sub(pref_start)
 
-  local items = {}
+    local items = {}
 
-  -- Luasnip format:
-  -- {
-  --  description = table(string),
-  --  name = string,
-  --  regTrig = bool,
-  --  trigger = string,
-  --  wordTrig = bool
-  -- }
-  local function add_snippet(s)
-    table.insert(items, {
-      word = s.trigger,
-      abbr = s.name,
-      kind = 'S',
-      menu = table.concat(s.description or {}),
-      icase = 1,
-      dup = 1,
-      empty = 1,
-      equal = 1,
-      user_data = { source = 'luasnip' },
-    })
-  end
-
-  vim.tbl_map(add_snippet, snippets.all)
-  vim.tbl_map(add_snippet, snippets[api.nvim_buf_get_option(0, 'filetype')])
-
-  return items, prefix
-end)
-
--- Shamelessly stollen from https://github.com/mfussenegger/nvim-lsp-compl with small adaptations
-M.lsp_matches = cached('lsp', function(line_to_cursor, lnum)
-  -- For lsp determining the preffix is painful, but thanks to the great @mfussenegger, we can fix
-  -- this all !
-  local function adjust_start_col(lnum, line, items, encoding)
-    local min_start_char = nil
-
-    for _, item in pairs(items) do
-      if item.textEdit and item.textEdit.range.start.line == lnum - 1 then
-        if min_start_char and min_start_char ~= item.textEdit.range.start.character then
-          return nil
-        end
-        min_start_char = item.textEdit.range.start.character
-      end
-    end
-    if min_start_char then
-      if encoding == 'utf-8' then
-        return min_start_char + 1
-      else
-        return vim.str_byteindex(line, min_start_char, encoding == 'utf-16') + 1
-      end
-    else
-      return nil
-    end
-  end
-
-  local params = lsp.util.make_position_params()
-  local result_all, err = lsp.buf_request_sync(0, 'textDocument/completion', params)
-  assert(not err, vim.inspect(err))
-  if not result_all then
-    return
-  end
-
-  local matches = {}
-  local start_col = vim.fn.match(line_to_cursor, '\\k*$') + 1
-  for client_id, result in pairs(result_all) do
-    local client = lsp.get_client_by_id(client_id)
-    local items = lsp.util.extract_completion_items(result.result)
-
-    local tmp_col = adjust_start_col(lnum, line_to_cursor, items, client.offset_encoding or 'utf-16')
-    if tmp_col and tmp_col < start_col then
-      start_col = tmp_col
-    end
-
-    for _, item in pairs(items or {}) do
-      local kind = lsp.protocol.CompletionItemKind[item.kind] or ''
-      local word
-      if kind == 'Snippet' then
-        word = item.label
-      elseif item.insertTextFormat == 2 then
-        if item.textEdit then
-          word = item.insertText or item.textEdit.newText
-        elseif item.insertText then
-          if #item.label < #item.insertText then
-            word = item.label
-          else
-            word = item.insertText
-          end
-        else
-          word = item.label
-        end
-      else
-        word = (item.textEdit and item.textEdit.newText) or item.insertText or item.label
-      end
-      item.client_id = client_id
-      item.source = 'lsp'
-      table.insert(matches, {
-        word = word,
-        abbr = item.label,
-        kind = kind,
-        menu = item.detail or '',
+    -- Luasnip format:
+    -- {
+    --  description = table(string),
+    --  name = string,
+    --  regTrig = bool,
+    --  trigger = string,
+    --  wordTrig = bool
+    -- }
+    local function add_snippet(s)
+      table.insert(items, {
+        word = s.trigger,
+        abbr = s.name,
+        kind = 'S',
+        menu = table.concat(s.description or {}),
         icase = 1,
         dup = 1,
         empty = 1,
         equal = 1,
-        user_data = item,
+        user_data = { source = 'luasnip' },
       })
     end
-  end
-  local prefix = line_to_cursor:sub(start_col)
-  return matches, prefix
-end)
+
+    source_ft = opts.filetype or api.nvim_buf_get_option(0, 'filetype')
+    if not opts.exclude_default then
+      vim.tbl_map(add_snippet, snippets.all)
+    end
+    vim.tbl_map(add_snippet, snippets[source_ft])
+
+    return items, prefix
+  end)
+end
+
+-- Shamelessly stollen from https://github.com/mfussenegger/nvim-lsp-compl with small adaptations
+function M.lsp_matches(opts)
+  return cached('lsp', function(line_to_cursor, lnum)
+    -- For lsp determining the preffix is painful, but thanks to the great @mfussenegger, we can fix
+    -- this all !
+    local function adjust_start_col(lnum, line, items, encoding)
+      local min_start_char = nil
+
+      for _, item in pairs(items) do
+        if item.textEdit and item.textEdit.range.start.line == lnum - 1 then
+          if min_start_char and min_start_char ~= item.textEdit.range.start.character then
+            return nil
+          end
+          min_start_char = item.textEdit.range.start.character
+        end
+      end
+      if min_start_char then
+        if encoding == 'utf-8' then
+          return min_start_char + 1
+        else
+          return vim.str_byteindex(line, min_start_char, encoding == 'utf-16') + 1
+        end
+      else
+        return nil
+      end
+    end
+
+    local params = lsp.util.make_position_params()
+    local result_all, err = lsp.buf_request_sync(0, 'textDocument/completion', params)
+    assert(not err, vim.inspect(err))
+    if not result_all then
+      return
+    end
+
+    local matches = {}
+    local start_col = vim.fn.match(line_to_cursor, '\\k*$') + 1
+    for client_id, result in pairs(result_all) do
+      local client = lsp.get_client_by_id(client_id)
+      local items = lsp.util.extract_completion_items(result.result)
+
+      local tmp_col = adjust_start_col(lnum, line_to_cursor, items, client.offset_encoding or 'utf-16')
+      if tmp_col and tmp_col < start_col then
+        start_col = tmp_col
+      end
+
+      for _, item in pairs(items or {}) do
+        local kind = lsp.protocol.CompletionItemKind[item.kind] or ''
+        local word
+        if kind == 'Snippet' then
+          word = item.label
+        elseif item.insertTextFormat == 2 then
+          if item.textEdit then
+            word = item.insertText or item.textEdit.newText
+          elseif item.insertText then
+            if #item.label < #item.insertText then
+              word = item.label
+            else
+              word = item.insertText
+            end
+          else
+            word = item.label
+          end
+        else
+          word = (item.textEdit and item.textEdit.newText) or item.insertText or item.label
+        end
+        item.client_id = client_id
+        item.source = 'lsp'
+        table.insert(matches, {
+          word = word,
+          abbr = item.label,
+          kind = kind,
+          menu = item.detail or '',
+          icase = 1,
+          dup = 1,
+          empty = 1,
+          equal = 1,
+          user_data = item,
+        })
+      end
+    end
+    local prefix = line_to_cursor:sub(start_col)
+    return matches, prefix
+  end)
+end
 
 local function apply_snippet(item, suffix)
   local luasnip = require 'luasnip'
